266d0eca02973c9171c6db9447ea3550679e054b
Jonathan <Jonathan_Wright@hotmail.com>
2020-04-07 23:02:42 +0800

    Working Main

--- program.c
+++ program.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <errno.h>
+#include <limits.h>
 int main(int argc, char** argv) {
     char* endptr;
     int bufferSize, liftTime;
-    if (argc != 3) {
-        bufferSize = strtol(argv[1], &endptr);
-        if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN)) || (errno != 0 && val == 0)) {
+    if (argc == 3) {
+        bufferSize = strtol(argv[1], &endptr, 10);
+        if ((errno == ERANGE && (bufferSize == LONG_MAX || bufferSize == LONG_MIN)) || (errno != 0 && bufferSize == 0)) {
             fprintf(stderr, "Invalid Number Entered for m: %s\n", argv[1]);
             exit(EXIT_FAILURE);
         }
@@ -14,15 +16,15 @@ int main(int argc, char** argv) {
             exit(EXIT_FAILURE);
         }
         if (*endptr != '\0')
-            printf("Further Digits were cut off frorm m as they couldnt be stored.");
-        liftTime = strtol(argv[2], &endptr);
-        if (endptr == argv[1]) {
+            printf("Further Digits were cut off from m as they couldnt be stored.");
+        liftTime = strtol(argv[2], &endptr, 10);
+        if (endptr == argv[2]) {
             fprintf(stderr, "No Digits were found for t.\n");
             exit(EXIT_FAILURE);
         }
         if (*endptr != '\0')
-            printf("Further Digits were cut off frorm m as they couldnt be stored.");
-        if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN)) || (errno != 0 && val == 0)) {
+            printf("Further Digits were cut off from t as they couldnt be stored.");
+        if ((errno == ERANGE && (liftTime == LONG_MAX || liftTime == LONG_MIN)) || (errno != 0 && liftTime == 0)) {
             fprintf(stderr, "Invalid Number Entered for m: %s\n", argv[1]);
             exit(EXIT_FAILURE);
         }
