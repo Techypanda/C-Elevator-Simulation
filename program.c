f1b1f4e9ae61e30ca94f27350eec985322c6c4fd
BuildTools <unconfigured@null.spigotmc.org>
2020-04-07 22:54:20 +0800

    Error Checking

--- program.c
+++ program.c
@@ -1,14 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 int main(int argc, char** argv) {
+    char* endptr;
     int bufferSize, liftTime;
     if (argc != 3) {
-        bufferSize = strtol(argv[1]);
+        bufferSize = strtol(argv[1], &endptr);
         if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN)) || (errno != 0 && val == 0)) {
             fprintf(stderr, "Invalid Number Entered for m: %s\n", argv[1]);
             exit(EXIT_FAILURE);
         }
-        liftTime = strtol(argv[2]);
+        if (endptr == argv[1]) {
+            fprintf(stderr, "No Digits were found for m.\n");
+            exit(EXIT_FAILURE);
+        }
+        if (*endptr != '\0')
+            printf("Further Digits were cut off frorm m as they couldnt be stored.");
+        liftTime = strtol(argv[2], &endptr);
+        if (endptr == argv[1]) {
+            fprintf(stderr, "No Digits were found for t.\n");
+            exit(EXIT_FAILURE);
+        }
+        if (*endptr != '\0')
+            printf("Further Digits were cut off frorm m as they couldnt be stored.");
         if ((errno == ERANGE && (val == LONG_MAX || val == LONG_MIN)) || (errno != 0 && val == 0)) {
             fprintf(stderr, "Invalid Number Entered for m: %s\n", argv[1]);
             exit(EXIT_FAILURE);
